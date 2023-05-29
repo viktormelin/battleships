c980f1eb170f7eac4c7c63cd438163d24d9f5faf
dough-land <viktor.melin@me.com>
2023-05-29 13:40:22 +0200

    refactor: remove stuff

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,63 +1,13 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import GameCardVue from './GameCard.vue'
-import { BoardState } from '../types'
-
-const props = defineProps<{
-  currentPlayer: string
-}>()
-
-const gameBoardStates = ref<BoardState[]>([
-  {
-    id: 1,
-    state: false
-  },
-  {
-    id: 2,
-    state: false
-  },
-  {
-    id: 3,
-    state: false
-  },
-  {
-    id: 4,
-    state: false
-  },
-  {
-    id: 5,
-    state: false
-  },
-  {
-    id: 6,
-    state: false
-  },
-  {
-    id: 7,
-    state: false
-  },
-  {
-    id: 8,
-    state: false
-  },
-  {
-    id: 9,
-    state: false
-  }
-])
-
-const handleClick = (id: number) => {
-  console.log(id, props.currentPlayer)
-
-  const cardIndex = gameBoardStates.value.findIndex((card) => card.id === id)
-  gameBoardStates.value[cardIndex].state = props.currentPlayer
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div id="board" class="grid grid-cols-3 gap-2">
-    <div v-for="card in gameBoardStates" :key="card.id">
-      <GameCardVue :id="card.id" :state="card.state" @handleClick.once="handleClick" />
+    <div v-for="card in 9" :key="card">
+      <div
+        class="relative bg-slate-700 h-[150px] w-[150px] rounded-sm flex items-center justify-center text-slate-300 cursor-pointer"
+      >
+        <p>{{ card }}</p>
+      </div>
     </div>
   </div>
 </template>
